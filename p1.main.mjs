cf033732f480b9107e173febea2df2049d58bff1
azam2730 <azamnishat2001@gmail.com>
2024-04-01 22:10:29 +0530

    changes

---
+++ p1.main.mjs
@@ -0,0 +1,5 @@
+import maths from './p1.maths_module.mjs';
+const add = maths.add(10,20);
+const sub = maths.sub(20,10);
+console.log('add ::',add);
+console.log('sub ::',sub);
---
+++ p2.main.mjs
@@ -0,0 +1,5 @@
+import maths from './p2.maths_module.mjs';
+const add = maths.add(10,20);
+const sub = maths.sub(20,10);
+console.log('add ::',add);
+console.log('sub ::',sub);
